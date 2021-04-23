f6d062919c54a4862d30ba2e8cfbe541eb149ec4
hanbee1005 <sonhanbi1002@gmail.com>
2021-04-23 14:22:31 +0900

    useInput hook 추가

--- src/useState/UseInput.js
+++ src/useState/UseInput.js
@@ -2,12 +2,24 @@ import React, { useState } from "react";
 
 import './style.css';
 
+const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = event => {
+        console.log(event.target);
+    };
+    return { value, onChange };
+};
+
 const UseInput = () => {
+    const name = useInput("Mr.");
     return (
         <div className="App">
             <h1>Hello</h1>
+            <input placeholder="Name" value={name.value} onChange={name.onChange}/>
         </div>
     );
 };
 
 export default UseInput;
+
+// {name.value} 는 {...name} 이라고 해도 가능
